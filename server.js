786311ad5e69fc4ec8bc6e111db5a085191a7898
VladGrushevoi <vlad.grush.99@gmail.com>
2020-06-18 15:36:58 +0300

    make it better

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const port = process.env.PORT || 3000;
-g
+
 app.get('/', function (request, res) {
     res.sendFile(path.join(__dirname, '/index.html'));
 });
